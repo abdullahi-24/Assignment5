fb82dd1e8edef1d9175a8a39c60bd62c7ef27be4
colinorth <colin.orth@live.com>
2023-12-04 14:36:11 -0600

    Created Browse Music page

---
+++ MvcMusicShop/Data/MvcMusicShopContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMusicShop.Models;
+
+namespace MvcMusicShop.Data
+{
+    public class MvcMusicShopContext : DbContext
+    {
+        public MvcMusicShopContext (DbContextOptions<MvcMusicShopContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMusicShop.Models.Music> Music { get; set; } = default!;
+    }
+}
---
+++ MvcMusicShop/Migrations/20231204202309_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MvcMusicShop.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Music",
+                columns: table => new
+                {
+                    Genre = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Music", x => x.Genre);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Music");
+        }
+    }
+}
